c7d06a8b9350d05095afd50014f366f0fe0b1f98
Luis Henrique <luishenrrique34@gmail.com>
2020-01-04 20:32:56 -0300

    adding shapes, background and features

--- Main.cpp
+++ Main.cpp
@@ -11,9 +11,7 @@ int field[M][N] = { 0 };
 struct Point
 {
     int x, y;
-} 
-
-a[4], b[4];
+} a[4], b[4];
 
 
 int figures[7][4] =
@@ -27,25 +25,52 @@ int figures[7][4] =
     2,3,4,5, // O
 };
 
+bool check()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) {
+            return 0;
+        }
+        else if (field[a[i].y][a[i].x]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
 int main() {
 
-    RenderWindow window(VideoMode(320, 480), "The Game!");
+    srand(time(0));
 
-    Texture t;
+    RenderWindow window(VideoMode(320, 480), "TETRIS The Game!");
+
+    Texture t, background, frame;
     t.loadFromFile("images/tiles.png");
+    background.loadFromFile("images/background.png");
+    frame.loadFromFile("images/frame.png");
+
+    Sprite s(t);
+    Sprite back(background);
+    Sprite f(frame);
+
+    s.setTextureRect(IntRect(0, 0, 18, 18));
 
     int dx = 0;
     bool rotate = 0;
     int colorNum = 1;
 
+    float timer = 0, delay = 0.3;
 
-
-    Sprite s(t);
-    s.setTextureRect(IntRect(0, 0, 18, 18));
-
+    Clock clock;
 
     while (window.isOpen())
     {
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
         Event e;
         while (window.pollEvent(e))
         {
@@ -58,14 +83,24 @@ int main() {
                 else if (e.key.code == Keyboard::Right) dx = 1;
         }
 
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+        {
+            delay = 0.05;
+        }
+
         ///// <<<<<< MOVE >>>>>>> ///
+        for (int i = 0; i < 4; i++) 
+        {
+            b[i] = a[i]; 
+            a[i].x += dx;
+        }
 
-        for (int i = 0; i < 4; i++) a[i].x += dx;
-
-
-
-
-
+        if (!check()) {
+            for (int i = 0; i < 4; i++)
+            {
+                a[i] = b[i];
+            }
+        }
 
         ///// <<<<<< ROTATE >>>>>>> ///
         if (rotate)
@@ -79,30 +114,96 @@ int main() {
                 a[i].x = p.x - x;
                 a[i].y = p.y + y;
             }
+
+            if (!check()) {
+                for (int i = 0; i < 4; i++)
+                {
+                    a[i] = b[i];
+                }
+            }
         }
 
-
-
-
-        int n = 3;
-        if(a[0].x == 0)
-        for (int i = 0; i < 4; i++)
+        ///// <<<<<< TICK >>>>>>> ///
+        if (timer > delay)
         {
-            a[i].x = figures[n][i] % 2;
-            a[i].y = figures[n][i] / 2;
+            for (int i = 0; i < 4; i++)
+            {
+                b[i] = a[i];
+                a[i].y += 1;
+            }
+
+            if (!check())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    field[b[i].y][b[i].x] = colorNum;
+                }
+
+                colorNum = 1 + rand() % 7;
+                int n = rand() % 7;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    a[i].x = figures[n][i] % 2;
+                    a[i].y = figures[n][i] / 2;
+                }
+            }
+
+            timer = 0;
+        }
+
+        ////// <<<< CHECK LINES >>>> ////
+        int k = M - 1;
+        for (int i = M - 1; i > 0; i--)
+        {
+            int count = 0;
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i][j]) {
+                    count++;
+                }
+                field[k][j] = field[i][j];
+            }
+
+            if (count < N)
+            {
+                k--;
+            }
         }
 
         dx = 0;
         rotate = 0;
+        delay = 0.3;
 
+        /////// <<<< DRAW >>>> //// 
         window.clear(Color::White);
+        window.draw(back);
+
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i][j] == 0)
+                {
+                    continue;
+                }
+
+                s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+                s.setPosition(j * 18, i * 18);
+                s.move(28, 31); //offset
+                window.draw(s);
+            }
+        }
 
         for (int i = 0; i < 4; i++)
         {
+            s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
             s.setPosition(a[i].x * 18, a[i].y * 18);
+            s.move(28, 31); // offset
             window.draw(s);
         }
         
+        window.draw(f);
         window.display();
     }
 
