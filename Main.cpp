715c439667bfe910f0b49cce4c4c6027dac3bf28
Luis Henrique <luishenrrique34@gmail.com>
2020-01-04 00:45:13 -0300

    Adding tile's and movements

---
+++ Main.cpp
@@ -0,0 +1,110 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+using namespace sf;
+
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = { 0 };
+
+struct Point
+{
+    int x, y;
+} 
+
+a[4], b[4];
+
+
+int figures[7][4] =
+{
+    1,3,5,7, // I
+    2,4,5,7, // Z
+    3,5,4,6, // S
+    3,5,4,7, // T
+    2,3,5,7, // L
+    3,5,7,6, // J
+    2,3,4,5, // O
+};
+
+int main() {
+
+    RenderWindow window(VideoMode(320, 480), "The Game!");
+
+    Texture t;
+    t.loadFromFile("images/tiles.png");
+
+    int dx = 0;
+    bool rotate = 0;
+    int colorNum = 1;
+
+
+
+    Sprite s(t);
+    s.setTextureRect(IntRect(0, 0, 18, 18));
+
+
+    while (window.isOpen())
+    {
+        Event e;
+        while (window.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
+                window.close();
+
+            if (e.type == Event::KeyPressed)
+                if (e.key.code == Keyboard::Up) rotate = true;
+                else if (e.key.code == Keyboard::Left) dx = -1;
+                else if (e.key.code == Keyboard::Right) dx = 1;
+        }
+
+        ///// <<<<<< MOVE >>>>>>> ///
+
+        for (int i = 0; i < 4; i++) a[i].x += dx;
+
+
+
+
+
+
+        ///// <<<<<< ROTATE >>>>>>> ///
+        if (rotate)
+        {
+            Point p = a[1]; //Center of rotation
+            for (int i = 0; i < 4; i++)
+            {
+                int x = a[i].y - p.y;
+                int y = a[i].x - p.x;
+
+                a[i].x = p.x - x;
+                a[i].y = p.y + y;
+            }
+        }
+
+
+
+
+        int n = 3;
+        if(a[0].x == 0)
+        for (int i = 0; i < 4; i++)
+        {
+            a[i].x = figures[n][i] % 2;
+            a[i].y = figures[n][i] / 2;
+        }
+
+        dx = 0;
+        rotate = 0;
+
+        window.clear(Color::White);
+
+        for (int i = 0; i < 4; i++)
+        {
+            s.setPosition(a[i].x * 18, a[i].y * 18);
+            window.draw(s);
+        }
+        
+        window.display();
+    }
+
+    return 0;
+}
